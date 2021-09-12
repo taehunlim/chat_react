b081091afb7179ac0debf136a417fe776d204a14
taehunlim <54699548+taehunlim@users.noreply.github.com>
2021-09-12 16:49:51 +0900

    send message

--- src/pages/ChatRoom.tsx
+++ src/pages/ChatRoom.tsx
@@ -2,14 +2,49 @@ import React, { useState } from 'react';
 import useChat from "../components/customHook/useChat";
 
 const ChatRoom = (props) => {
+
     const { roomId } = props.match.params;
     const { messages, sendMessage } = useChat(roomId);
+    const [newMessage, setNewMessage] = useState("");
+
+    const handleNewMessageChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setNewMessage(e.target.value);
+    };
+
+    const handleSendMessage = () => {
+      sendMessage(newMessage);
+      setNewMessage("");
+    };
 
     return (
         <div>
             <h1 className="room-name">
                 Room: {roomId}
             </h1>
+
+            <div className="messages-container">
+                <ul className="messages-list">
+                    {messages.map((message, i) => (
+                        <li
+                            key={i}
+                            className={`message-item ${
+                                message.ownedByCurrentUser ? "my-message" : "received-message"
+                            }`}
+                        >
+                            {message.body}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <textarea
+                value={newMessage}
+                onChange={handleNewMessageChange}
+                placeholder="Write message..."
+                className="new-message-input-field"
+            />
+            <button onClick={handleSendMessage} className="send-message-button">
+                Send
+            </button>
         </div>
     );
 };
